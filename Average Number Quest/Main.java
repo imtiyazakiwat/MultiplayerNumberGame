2bb133ac4574176ddb2e5a2c1fb56c0dd7712b4b
imtiyazakiwat <94955223+imtiyazakiwat@users.noreply.github.com>
2023-01-21 13:25:03 +0530

    Update Main.java

--- Average Number Quest/Main.java
+++ Average Number Quest/Main.java
@@ -1,11 +1,10 @@
-package Game;
-
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 import java.lang.Math;
+import java.util.Scanner;
 
-public class Main {
+public class Main2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of players: ");
@@ -14,85 +13,83 @@ public class Main {
         String[] playerNames = new String[numPlayers];
         int[] scores = new int[numPlayers];
         int[] numbers = new int[numPlayers];
-        int[] roundNumber = new int[numPlayers];
+        int round;
         int avg;
         int minIndex;
         int minScore;
-        int maxIndex = -1;
+        int maxIndex = 0;
         int maxScore = 0;
+        BufferedWriter bw = null;
+        FileWriter fw = null;
 
         for (int i = 0; i < numPlayers; i++) {
             System.out.println("Enter player " + (i + 1) + " name: ");
             playerNames[i] = sc.nextLine();
         }
-        while (numPlayers > 1) {
-            for (int round = 1; round <= 3; round++) {
-                System.out.println("Welcome to round " + round + " of the game...");
-                for (int i = 0; i < numPlayers; i++) {
-                    if (playerNames[i] != null) {
+        try {
+            fw = new FileWriter("scores.txt");
+            bw = new BufferedWriter(fw);
+            while (numPlayers > 1) {
+                for (round = 1; round <= 3; round++) {
+                    System.out.println("Welcome to round " + round + " of the game...");
+                    for (int i = 0; i < numPlayers; i++) {
                         System.out.println(playerNames[i] + ", choose any number between 1 to 100: ");
                         numbers[i] = sc.nextInt();
                         sc.nextLine();
-                        roundNumber[i] = round;
-
-                        try {
-                            try (FileWriter writer = new FileWriter("scores.txt", true)) {
-                                for (i = 0; i < playerNames.length; i++) {
-                                    if (playerNames[i] != null) {
-                                        writer.write(playerNames[i] + " scored " + scores[i] + "in round "
-                                                + roundNumber[i] + "\n");
-                                    }
-                                }
-                                writer.close();
-                                // System.out.println("Scores saved to scores.txt");
-                            }
-                        } catch (IOException e) {
-                            System.out.println("An error occurred while saving scores to scores.txt");
-                            e.printStackTrace();
-                        }
+                    }
+                    avg = 0;
+                    for (int i = 0; i < numPlayers; i++) {
+                        avg += numbers[i];
+                    }
+                    avg /= numPlayers;
+                    for (int i = 0; i < numPlayers; i++) {
+                        int diff = Math.abs(numbers[i] - avg);
+                        scores[i] += 100 - diff;
+                    }
+                    for (int i = 0; i < numPlayers; i++) {
+                        bw.write(playerNames[i] + " : Round " + round + " : " + scores[i] + "\n");
                     }
                 }
-                avg = 0;
+                minIndex = 0;
+                minScore = Integer.MAX_VALUE;
                 for (int i = 0; i < numPlayers; i++) {
-                    avg += numbers[i];
-                }
-                avg /= numPlayers;
-                for (int i = 0; i < numPlayers; i++) {
-                    int diff = Math.abs(numbers[i] - avg);
-                    scores[i] += 100 - diff;
-                    if (scores[i] > maxScore) {
-                        maxScore = scores[i];
+                    if (scores[i] < minScore) {
+                        minScore = scores[i];
+                        minIndex = i;
                     }
                 }
-            }
-            minIndex = 0;
-            minScore = Integer.MAX_VALUE;
-            for (int i = 0; i < numPlayers; i++) {
-                if (scores[i] < minScore) {
-                    minScore = scores[i];
-                    minIndex = i;
-                }
-            }
-            if (playerNames[minIndex] != null) {
                 System.out.println(
                         "Player " + playerNames[minIndex] + " has been eliminated with a score of " + scores[minIndex]);
+                // Shift the remaining players in the array to the left
+                System.arraycopy(playerNames, minIndex + 1, playerNames, minIndex, numPlayers - minIndex - 1);
+                System.arraycopy(scores, minIndex + 1,scores, minIndex, numPlayers - minIndex - 1);
+                numPlayers--;
+                // Find the index of the maximum score
+                maxIndex = 0;
+                maxScore = 0;
                 for (int i = 0; i < numPlayers; i++) {
-                    if (playerNames[i] != null) {
-                        System.out.println(playerNames[i] + " has a score of " + scores[i]);
-                    }
+                if (scores[i] > maxScore) {
+                maxScore = scores[i];
+                maxIndex = i;
                 }
-            }
-            for (int i = 0; i < numPlayers; i++) {
-                if (playerNames[i] != null && scores[i] == maxScore) {
-                    System.out.println("The winner is " + playerNames[i] + " with a score of " + scores[i]);
                 }
-            }
-            playerNames[minIndex] = null;
-            scores[minIndex] = 0;
-            roundNumber[minIndex] = 0;
-            numPlayers--;
-        }
-
-        sc.close();
-    }
-}
+                }
+                System.out.println("The winner is " + playerNames[maxIndex] + " with a score of " + maxScore);
+                bw.write("The winner is " + playerNames[maxIndex] + " with a score of " + maxScore);
+                } catch (IOException e) {
+                e.printStackTrace();
+                } finally {
+                try {
+                if (bw != null)
+                bw.close();
+                if (fw != null)
+                fw.close();
+                } catch (IOException ex) {
+                ex.printStackTrace();
+                }
+                sc.close();
+                }
+                }
+                }
+                
+                
